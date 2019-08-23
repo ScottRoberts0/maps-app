9e6fb61a6eba9b336d32dde61f7dbf10f447c9c4
Scott Roberts <sroberts.kia@gmail.com>
2019-08-23 22:43:40 +0000

    added leatlet maps to homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,41 @@
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     console.log(users);
+//     for(user of users.users) {
+//       $("<div>").text(user.email).appendTo($("body"));
+//     }
+//   });;
+// });
+
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    console.log(users);
-    for(user of users.users) {
-      $("<div>").text(user.email).appendTo($("body"));
+    url: "/api/maps"
+  }).done((maps) => {
+    console.log(maps);
+
+    for(map of maps.maps) {
+      //let str = `${map.id}, ${map.lat}, ${map.long}, ${map.city}, ${map.user_id}`;
+      //$("<div>").text(str).appendTo($("body"));
+      $("<div>").attr('id', 'map'+map.id).css({"width": "900px", "height": "580px"}).appendTo($("body"));
+
+      var mapOptions = {
+        center: [map.lat, map.long],
+        zoom: 10
+     }
+
+     // Creating a map object
+     var map = new L.map('map'+map.id, mapOptions);
+
+     // Creating a Layer object
+     var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+     // Adding layer to the map
+     map.addLayer(layer);
+
     }
   });;
 });
