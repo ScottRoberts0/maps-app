1c6dcdf067a9e5ba66a4633f065001318b1c5918
Scott Roberts <sroberts.kia@gmail.com>
2019-08-24 18:31:10 +0000

    added new seeds for vancouver

--- db/seeds/03_markers.sql
+++ db/seeds/03_markers.sql
@@ -1,3 +1,28 @@
 -- Markers table seeds here (Example)
-INSERT INTO markers (lat, long, title, description, img, map_id) VALUES (51.037999, -114.093964, 'McDonalds', 'Im loving it!', 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwilxPOi_ZnkAhWPtZ4KHT62BRYQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.rd.com%2Ffood%2Ffun%2Fmcdonalds-gold-card%2F&psig=AOvVaw3zdEvsFIsatD6Avp7Zvyo4&ust=1566683571993500', 1);
-INSERT INTO markers (lat, long, title, description, img, map_id) VALUES (51.033705, -114.071316, 'Anejo', 'This is a restaurant', 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwilxPOi_ZnkAhWPtZ4KHT62BRYQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.rd.com%2Ffood%2Ffun%2Fmcdonalds-gold-card%2F&psig=AOvVaw3zdEvsFIsatD6Avp7Zvyo4&ust=1566683571993500', 1);
+INSERT INTO markers (lat, long, title, description, img, map_id)
+VALUES (
+  51.037999,
+  -114.093964,
+  'McDonalds',
+  'Im loving it!',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png',
+   1);
+INSERT INTO markers (lat, long, title, description, img, map_id)
+VALUES (
+  51.033705,
+  -114.071316,
+  'Anejo',
+  'This is a restaurant',
+  'https://s3.amazonaws.com/btoimage/prism-thumbnails/articles/20171013-aloette-08.jpg-resize_then_crop-_frame_bg_color_FFF-h_480-gravity_center-q_70-preserve_ratio_true-w_720_.webp',
+  1);
+
+  INSERT INTO markers (lat, long, title, description, img, map_id)
+VALUES (
+  49.251159,
+  -123.154072,
+  'KFC',
+  'Finger Lickin GOod',
+  'https://www.joc.com/sites/default/files/field_feature_image/KFC_0.png',
+  2);
+
+
