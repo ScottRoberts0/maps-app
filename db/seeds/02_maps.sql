3e891773247a73f7f2f27839adaf95881f224217
Scott Roberts <sroberts.kia@gmail.com>
2019-08-26 17:56:25 +0000

    added title and img to maps in database

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,3 +1,3 @@
 -- Maps table seeds here (Example)
-INSERT INTO maps (lat, long, city, user_id) VALUES (51.044270, -114.062019, 'Calgary', 1);
-INSERT INTO maps (lat, long, city, user_id) VALUES (49.2827, -123.1207, 'Vancouver', 2);
+INSERT INTO maps (lat, long, city, title, img, user_id) VALUES (51.044270, -114.062019, 'Calgary', 'Best Restaurants', 'https://cdn-blog.queensland.com/wp-content/uploads/2018/03/gold-coast-restaurants-with-epic-water-views.jpg',  1);
+INSERT INTO maps (lat, long, city, title, img, user_id) VALUES (49.2827, -123.1207, 'Vancouver', 'Parks', 'https://www.surrey.ca/images/cos-master/pageImages/Hawthorne%20Park.JPG', 2);
